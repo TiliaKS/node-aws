846c6b973f2b64d5db49fece59fd9c6433edcace
Serhii Kotelevets <serhii_kotelevets@epam.com>
2023-12-09 15:18:10 +0200

    Trigger Build

--- app/index.js
+++ app/index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const PORT = process.env.APP_PORT ?? 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Hello World with NODE JS');
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running http://localhost:${PORT}`)
-})
+  console.log(`Server is running http://localhost:${PORT}`);
+});
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+//
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// app.listen(PORT, () => {
+//   console.log(`Server is running http://localhost:${PORT}`)
+// })
